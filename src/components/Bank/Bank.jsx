c6f5d95cc12e413a18b3154b53d30745c832b7e5
Mikee Ojastro <mikeeojastro@gmail.com>
2025-04-04 20:20:10 +0800

    April 4, 2025

--- src/components/Bank/Bank.jsx
+++ src/components/Bank/Bank.jsx
@@ -10,15 +10,58 @@ function Bank() {
     const [amount, setAmount] = useState("");
   
     const userExist = (name) => {
-
+      // find
+      // returns a boolean value given the condition
+      return users.find((user) => user.name === name);
     }
   
     const findUser = (name) => {
-
+      // filter method - returns an array
+      // it should match the condition inside the checker
+      let foundUser = users.filter((user) => user.name === name);
+      return foundUser[0];
     }
   
     const transferMoney = () => {
+      // changing amount to number
+      const newAmount = Number(amount);
+      // are the users existing?
+      // sender is not sending to himself
+      // newAmount is valid
+      if(userExist(sender) && userExist(receiver) && sender !== receiver && newAmount > 0){
+        const senderInfo = findUser(sender);
+        if(senderInfo.balance >= newAmount){
+          // update our users array
+          // new balance of sender and receiver should be reflected
+          // user - each element in our users array
+          // map - returns a new array with the updated details
+          // looping through our users array
+          const updateUsers = users.map((user) => {
+            if(user.name === sender){
+              return {
+                ...user,
+                balance: user.balance - newAmount
+              };
+            } else if(user.name === receiver){
+              return {
+                ...user,
+                balance: user.balance + newAmount
+              };
+            }
+            return user;
+          });
+          setUsers(updateUsers);
+        } else {
+          alert('Not enough balance');
+        }
+      } else {
+        alert('Transaction invalid');
+      }
 
+      setSender('');
+      setReceiver('');
+      setAmount('');
+      setShow(false);
     }
 
     return (
